78f5975ed7732b53152c7d0984f8882ed4dbfb5d
MohammadReza Najafi <mrnj92@gmail.com>
2018-08-23 15:51:11 +0430

    saved sorted citites in a file for later use

--- app/src/main/java/com/backbase/interview/citieslist/main/MainActivity.java
+++ app/src/main/java/com/backbase/interview/citieslist/main/MainActivity.java
@@ -19,6 +19,10 @@ import com.backbase.interview.citieslist.ui.activities.BaseActivity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
@@ -101,16 +105,87 @@ public class MainActivity extends BaseActivity {
     }
 
     private long lastNotifiedProgress = 0;
+
     @Override protected void onProgressUpdate(Integer... values) {
       super.onProgressUpdate(values);
 
       if (System.currentTimeMillis() - lastNotifiedProgress > 5_000) {
         lastNotifiedProgress = System.currentTimeMillis();
-        Timber.d("progress:[%s:%s]", values[0], (values[0]/209557f)*100);
-
+        Timber.d("progress:[%s:%s]", values[0], (values[0] / 209557f) * 100);
       }
     }
 
+    private List<City> deserializeParse(JsonReader jsonReader) throws IOException {
+      final List<City> citiesList = new LinkedList<>();
+      final Gson gson = new GsonBuilder().create();
+      jsonReader.beginArray();
+      final long processStartTime = System.nanoTime();
+      while (jsonReader.hasNext()) {
+        final City currentCity = gson.fromJson(jsonReader, City.class);
+        citiesList.add(currentCity);
+      }
+      Timber.w("deserializeParse() #%s items in duration:[%sms]", citiesList.size(),
+          TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - processStartTime));
+
+      jsonReader.endArray();
+      jsonReader.close();
+
+      return citiesList;
+    }
+
+    private List<City> typeTokenParse(JsonReader jsonReader) throws IOException {
+      final List<City> citiesList = new LinkedList<>();
+
+      jsonReader.beginArray();
+
+      final long processStartTime = System.nanoTime();
+      while (jsonReader.hasNext()) {
+
+        jsonReader.beginObject();
+        jsonReader.nextName();
+        final String country = jsonReader.nextString();
+
+        jsonReader.nextName();
+        final String name = jsonReader.nextString();
+
+        jsonReader.nextName();
+        jsonReader.nextInt();
+
+        jsonReader.nextName();
+        jsonReader.beginObject();
+
+        jsonReader.nextName();
+        final double longitude = jsonReader.nextDouble();
+        jsonReader.nextName();
+        final double latitude = jsonReader.nextDouble();
+        jsonReader.endObject();
+        jsonReader.endObject();
+
+        citiesList.add(City.from(name, country, Coordination.from(latitude, longitude)));
+      }
+      Timber.w("typeTokenParse() #%s items in duration:[%sms]", citiesList.size(),
+          TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - processStartTime));
+
+      jsonReader.endArray();
+      jsonReader.close();
+
+      return citiesList;
+    }
+
+    private void sortAsc(final List<City> citiesList) {
+      final long sortStartTime = System.nanoTime();
+      Collections.sort(citiesList, new Comparator<City>() {
+        @Override public int compare(City o1, City o2) {
+          int cityNameCompare = o1.name.compareTo(o2.name);
+          if (cityNameCompare != 0) return cityNameCompare;
+
+          return o1.country.compareTo(o2.country);
+        }
+      });
+
+      Timber.w("sortAsc() took:[%sms]",
+          TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - sortStartTime));
+    }
     @Override protected List<City> doInBackground(Void... voids) {
       Timber.w("doInBackground");
 
@@ -121,51 +196,59 @@ public class MainActivity extends BaseActivity {
         final InputStream inputStream = mContextWeakReference.get().getAssets().open("cities.json");
         final JsonReader jsonReader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
 
+        //citiesList.addAll(deserializeParse(jsonReader));
+        citiesList.addAll(typeTokenParse(jsonReader));
+        sortAsc(citiesList);
+        saveSortedCitiesList(citiesList);
 
-        final Gson gson = new GsonBuilder().create();
 
-        int currentIndex = 0;
-
-
-        jsonReader.beginArray();
-
-        final long processStartTime = System.nanoTime();
-        while (jsonReader.hasNext()) {
-
-          final City currentCity = gson.fromJson(jsonReader, City.class);
-          citiesList.add(currentCity);
-          //publishProgress(currentIndex++);
-
-          //publishProgress(currentIndex++);
-          //currentIndex++;
-          //if (currentIndex >= 1000) {
-          //  break;
-          //}
-        }
-        Timber.w("processed:[%s records] in duration:[%s]",citiesList.size(),
-            TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - processStartTime));
-
-        jsonReader.endArray();
-        jsonReader.close();
-
-        final long sortStartTime = System.nanoTime();
-        Collections.sort(citiesList, new Comparator<City>() {
-          @Override public int compare(City o1, City o2) {
-            int cityNameCompare = o1.name.compareTo(o2.name);
-            if(cityNameCompare != 0)
-            return cityNameCompare;
-
-            return o1.country.compareTo(o2.country);
-          }
-        });
-
-        Timber.w("sort_duration:[%s]",
-            TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - sortStartTime));
       } catch (Exception ex) {
         Timber.e(ex, "while parse");
         //ex.printStackTrace();
       }
       return citiesList;
+    }
+
+    private void saveSortedCitiesList(List<City> citiesList) {
+
+      JsonWriter writer;
+
+
+      final long writeStartTime = System.nanoTime();
+
+      try {
+        if(mContextWeakReference.get() == null) return;
+        File sortedCitiesFile =
+            new File(mContextWeakReference.get().getExternalCacheDir(), "sorted_cities.json");
+
+        if (sortedCitiesFile.exists() && sortedCitiesFile.length() > 0) {
+          return;
+        }
+        writer = new JsonWriter(new FileWriter(
+            sortedCitiesFile));
+        writer.beginArray();
+
+        for (City city : citiesList) {
+          writer.beginObject();
+          writer.name("country").value(city.country);
+          writer.name("name").value(city.name);
+          writer.name("coord");
+          writer.beginObject();
+          writer.name("lon").value(city.coord.lon);
+          writer.name("lat").value(city.coord.lat);
+          writer.endObject();
+          writer.endObject();
+        }
+
+        Timber.w("saveSortedCitiesList() #%s items took [%sms] in:[%s]", citiesList.size(),
+            TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - writeStartTime), sortedCitiesFile.getAbsolutePath());
+        writer.endArray();
+        writer.close();
+
+
+      } catch (IOException e) {
+        System.err.print(e.getMessage());
+      }
     }
 
     @Override protected void onPostExecute(List<City> cities) {
